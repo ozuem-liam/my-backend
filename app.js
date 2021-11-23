a8dc499cd66a76906364212e2c84e121ef8a2638
ozuem-liam <ozuemdw@gmail.com>
2021-11-23 12:58:03 +0100

    feat: setting project server

--- app.js
+++ app.js
@@ -1,2 +1,27 @@
-const a = 1
+require('dotenv').config();
+const express = require('express'),
+  cors = require('cors');
 
+const app = express();
+
+app.use(express.json({ limit: '50mb' }));
+
+app.get('/', (request, response) => {
+  response.send('WELCOME');
+});
+
+const corsOptions = {
+  origin: '*',
+  optionsSuccessStatus: 200,
+  allowedHeaders: ['Content-Type', 'Authorization'],
+  credentials: true,
+};
+
+app.use(cors(corsOptions));
+
+const PORT = process.env.PORT || 5000;
+
+// Run the server!
+app.listen(PORT, () =>
+  console.log(`Server running on port ${PORT}`)
+);
