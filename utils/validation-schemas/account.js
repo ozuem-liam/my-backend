3be0edf600694d4dae7e3a808cf569b9cfc70cd8
ozuem-liam <ozuemdw@gmail.com>
2021-11-25 09:15:22 +0100

    feat: updated registeration schema validattion

--- utils/validation-schemas/account.js
+++ utils/validation-schemas/account.js
@@ -4,16 +4,24 @@ const { body } = require('express-validator'),
 const registrationSchema = [
   body('email')
     .notEmpty()
-    .withMessage({ 'any.required': messages['ACT-EMAIL-REQUIRED'] }).bail()
+    .withMessage({ 'any.required': messages['ACT-EMAIL-REQUIRED'] })
+    .bail()
     .isEmail()
     .withMessage({ 'string.email': messages['ACT-EMAIL-INVALID'] }),
   body('first_name').notEmpty().withMessage({ 'any.required': messages['ACT-FIRSTNAME-REQUIRED'] }),
   body('last_name').notEmpty().withMessage({ 'any.required': messages['ACT-LASTNAME-REQUIRED'] }),
   body('password')
     .notEmpty()
-    .withMessage({ 'any.required': messages['ACT-PASSWORD-REQUIRED'] }).bail()
+    .withMessage({ 'any.required': messages['ACT-PASSWORD-REQUIRED'] })
+    .bail()
     .isLength({ min: 8 })
     .withMessage({ 'string.pattern.base': messages['ACT-PASSWORD-TOO-SHORT'] }),
+  body('confirm_password')
+    .notEmpty()
+    .withMessage({ 'any.required': messages['ACT-CONFIRM-PASSWORD-REQUIRED'] })
+    .bail()
+    .equals(body('password'))
+    .withMessage({ 'any.only': messages['ACT-CONFIRM-PASSWORD-MATCH'] }),
 ];
 
 module.exports = registrationSchema;
