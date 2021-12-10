980a3f1ae518fe4b3030c4a6abc956af39ac4303
ozuem-liam <ozuemdw@gmail.com>
2021-12-10 10:04:31 +0100

    feat: added messages

--- helpers/routes.js
+++ helpers/routes.js
@@ -3,6 +3,7 @@ const pspOperatorRoutes = require('../apis/pspOperatorModule/psp.operator.route'
 const tariffRoutes = require('../apis/tariffModule/tariff.route');
 const facilityRoutes = require('../apis/facilityModule/facility.route');
 const categoryRoutes = require('../apis/categoryModule/category.route');
+const invoiceRoutes = require('../apis/invoiceModule/invoice.route');
 
 module.exports = function (app) {
   app.use('/account', accountRoutes);
@@ -10,4 +11,5 @@ module.exports = function (app) {
   app.use('/tariff', tariffRoutes);
   app.use('/facility', facilityRoutes);
   app.use('/category', categoryRoutes);
+  app.use('/invoice', invoiceRoutes);
 };
