c38acd0414a007cd9fa32fdea29b0aa99ed9f89d
ozuem-liam <ozuemdw@gmail.com>
2021-12-11 22:26:13 +0100

    feat: remove required fields in facilities

--- utils/validation-schemas/facility.js
+++ utils/validation-schemas/facility.js
@@ -3,46 +3,27 @@ const messages = require('../../translation/messages.json');
 
 const FacilityRegistrationSchema = [
     body('facility_email_1')
-      .notEmpty()
-      .withMessage({ 'any.required': messages['FACILITY-EMAIL-REQUIRED'] })
-      .bail()
       .isEmail()
       .withMessage({ 'string.email': messages['FACILITY-EMAIL-INVALID'] }),
     body('facility_email_2')
-      .notEmpty()
-      .withMessage({ 'any.required': messages['FACILITY-EMAIL-REQUIRED'] })
-      .bail()
       .isEmail()
       .withMessage({ 'string.email': messages['FACILITY-EMAIL-INVALID'] }),
     body('facility_name').notEmpty().withMessage({ 'any.required': messages['FACILITY-NAME-REQUIRED'] }),
-    body('facility_phone_number_1')
-    .notEmpty()
-    .withMessage({ 'any.required': messages['ACT-PHONE-NUMBER-REQUIRED'] }),
-    body('facility_phone_number_2')
-    .notEmpty()
-    .withMessage({ 'any.required': messages['ACT-PHONE-NUMBER-REQUIRED'] }),
-    body('location').notEmpty().withMessage({ 'any.required': messages['FACILITY-LOCATION-REQUIRED'] }),
-    body('address').notEmpty().withMessage({ 'any.required': messages['FACILITY-ADDRESS-REQUIRED'] }),
-    body('charge_per_trip')
-      .notEmpty()
-      .withMessage({ 'any.required': messages['FACILITY-CHARGE-PER-TRIP-REQUIRED'] }),
-    body('number_of_trips')
-      .notEmpty()
-      .withMessage({ 'any.required': messages['FACILITY-NUMBER-OF-TRIPS-REQUIRED'] }),
-    body('number_of_bins')
-      .notEmpty()
-      .withMessage({ 'any.required': messages['FACILITY-NUMBER-OF-BINS-REQUIRED'] }),
-    body('service_charge')
-      .notEmpty()
-      .withMessage({ 'any.required': messages['FACILITY-SERVICE-CHARGE-REQUIRED'] }),
+    body('facility_phone_number_1'),
+    body('facility_phone_number_2'),
+    body('location'),
+    body('address'),
+    body('charge_per_trip'),
+    body('number_of_trips'),
+    body('number_of_bins'),
+    body('service_charge'),
     body('status')
-      .notEmpty()
-      .withMessage({ 'any.required': messages['FACILITY-STATUS-REQUIRED'] })
       .isIn(['Active', 'Completed'])
       .withMessage({ 'any.required': messages['FACILITY-STATUS-DO-NOT-EXIST'] }),
-    body('servicing_psp')
-      .notEmpty()
-      .withMessage({ 'any.required': messages['FACILITY-SERVICING-PSP-REQUIRED'] }),
+    body('billing_type')
+      .isIn(['PER TRIP ', 'MONTHLY'])
+      .withMessage({ 'any.required': messages['FACILITY-STATUS-DO-NOT-EXIST'] }),
+    body('servicing_psp'),
   ];
   
 
